5c4940606b1df8f0daffeb3d1f63d03bf581fbe6
dmitrypintusov <dzmitrypintusov@gmail.com>
2017-02-17 16:59:04 +0300

    changes in web

---
+++ Project/web/src/main/webapp/WEB-INF/resources/js/login_form_validation.js
@@ -0,0 +1,34 @@
+function validateForm()
+{
+	var flag = true;
+	var messageLogin = document.getElementById("emptyLogin");
+	var messagePassword = document.getElementById("emptyPassword");
+	var login = document.getElementById("login").value;
+	var password = document.getElementById().value;
+	var text = "" + "<br>";
+
+	if (getCookie('LocaleCookie') == 'en') {
+		text = "Field is empty";
+	} else {
+		text = "Поле не заполнено";
+	}
+
+	if (login == "") {
+		messageLogin.innerHTML = text;
+		flag = false;
+	} else {
+		messageLogin.innerHTML = "";
+	}
+	if (password == "") {
+		messagePassword.innerHTML = text;
+		flag = false;
+	} else {
+		messagePassword.innerHTML = "";
+	}
+	return flag;
+}
+
+function getCookie(name){
+	var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
