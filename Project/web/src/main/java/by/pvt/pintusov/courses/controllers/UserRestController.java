239fedac833ef492ae752a25669993474bc1a069
dmitrypintusov <dzmitrypintusov@gmail.com>
2017-02-22 17:15:09 +0300

    changes in web

--- Project/web/src/main/java/by/pvt/pintusov/courses/controllers/UserRestController.java
+++ Project/web/src/main/java/by/pvt/pintusov/courses/controllers/UserRestController.java
@@ -7,10 +7,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,5 +39,61 @@ private static Logger logger = Logger.getLogger(UserRestController.class);
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<User>
+	public ResponseEntity<User> getUser (@PathVariable("id") Integer id) {
+		User user = null;
+		try {
+			user = userService.getById(id);
+			if (user == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		}catch (ServiceException e) {
+			logger.error(e);
+		}
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<Integer> saveUser (User user) {
+		Integer userId = null;
+		try {
+			userId = (Integer) userService.saveOrUpdate(user);
+		}catch (ServiceException e) {
+			logger.error(e);
+		}
+		return new ResponseEntity<>(userId, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	@ResponseBody
+	public ResponseEntity<Integer> updateUser (@PathVariable ("id") Integer id, @RequestBody User newUser) {
+		Integer userId = null;
+		User user = null;
+		try {
+			user = userService.getById(id);
+			if (user == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+			user.setFirstName(newUser.getFirstName());
+			user.setLastName(newUser.getLastName());
+			user.setLogin(newUser.getLogin());
+			user.setPassword(newUser.getPassword());
+			user.setCourses(newUser.getCourses());
+			user.setMarks(newUser.getMarks());
+			user.setAccessLevels(newUser.getAccessLevels());
+			userId = (Integer) userService.saveOrUpdate(user);
+		}catch (ServiceException e) {
+			logger.error(e);
+		}
+		return new ResponseEntity<>(userId, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteUser (@PathVariable("id") Integer id) {
+		try {
+			userService.delete(id);
+		}catch (ServiceException e) {
+			logger.error(e);
+		}
+	}
 }
